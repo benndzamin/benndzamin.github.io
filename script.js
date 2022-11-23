3e15a42e4a189557eeaabad2973cf7b9a7efa274
benndzamin <88112537+benndzamin@users.noreply.github.com>
2022-11-23 01:30:01 +0100

    finished

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 $(document).ready(function () {
   const endpoint =
     "https://raw.githubusercontent.com/russ666/all-countries-and-cities-json/master/countries.json";
-
+  
   const addItems = document.querySelector(".add-items");
   let firstName = document.querySelector("#firstName");
   let lastName = document.querySelector("#lastName");
@@ -12,16 +12,20 @@ $(document).ready(function () {
   let selectCity = document.querySelector("#selectCity");
   let data = JSON.parse(localStorage.getItem("data")) || [];
   let id = (Date.now() + Math.random()).toFixed();
-  let tabela = document.querySelector("#example");
+  let table;
 
   let editFirstName = document.querySelector("#editFirstName");
   let editLastName = document.querySelector("#editLastName");
   let editEmail = document.querySelector("#editEmail");
   let editCountry = document.querySelector("#editSelectCountry");
   let editCity = document.querySelector("#editSelectCity");
+  let obj;
+  let city;
 
   //save data to localhost
   function saveData(e) {
+    e.preventDefault();
+    id++;
     const item = {
       id: id,
       name: firstName.value,
@@ -34,27 +38,30 @@ $(document).ready(function () {
     data.push(item);
     localStorage.setItem("data", JSON.stringify(data));
     this.reset();
+    table.clear().rows.add(data).draw();
+    $("#exampleModalCenter .close").click();
   }
   addItems.addEventListener("submit", saveData);
 
   //show datatable on page
   function showData() {
-    var dataSet = [];
-    for (let x in data) {
-      dataSet.push(Object.values(data[x]));
-    }
-    tabela = $("#example").DataTable({
-      data: dataSet,
+    table = $("#example").DataTable({
+      order: [[0, "desc"]],
+      data: data,
       columns: [
-        { title: "ID" },
-        { title: "Name" },
-        { title: "Last Name" },
-        { title: "e-mail" },
-        { title: "Country" },
-        { title: "City" },
+        //here we can use the properties from the data
+        { data: "id", title: "ID" },
+        { data: "name", title: "First name" },
+        { data: "surname", title: "Last name" },
+        { data: "email", title: "e-mail" },
+        { data: "country", title: "Country" },
+        { data: "city", title: "City" },
         {
+          data: "Action",
           title: "Action",
-          render: function (e) {
+          //you should pass the id of an item to the editButton here, so that you can get it when opening a modal (for delete or data saving)
+          //instead of adding event listeners below on each button.
+          render: function () {
             return `<div class="btn-group"> 
             <button type="button" name="edit" class="btn btn-outline-primary btn-sm edit" data-toggle="modal" data-target="#editModal">Edit</button>
             <button type="button" name="delete" class="btn btn-outline-danger btn-sm delete" >Delete</button>
@@ -63,36 +70,29 @@ $(document).ready(function () {
         },
       ],
     });
-    return tabela;
+    return table;
   }
   showData();
 
-  let editBtn = document.querySelectorAll(".edit");
-  let deleteBtn = document.querySelectorAll(".delete");
-  let updateBtn = document.querySelector(".update-changes");
-
   //edit data in localhost and update values in modal
-  function callUpdate() {
-    let id =
-      this.parentElement.parentElement.parentElement.firstChild.textContent;
+  function callUpdate(e) {
+    e.preventDefault();
+    let id = table.row(this.parentElement.parentElement).data().id;
     let index;
-
     data.forEach((item) => {
-      console.log(item.id);
       if (item.id == id) {
         index = data.indexOf(item);
       }
     });
 
-    editFirstName.value = data[index].name;
-    editLastName.value = data[index].surname;
-    editEmail.value = data[index].email;
-    editCountry.value = data[index].country;
-    editCity.value = data[index].city;
-    console.log(editCountry.value);
-    console.log(editCity.value);
-
-    function updateChanges() {
+    editFirstName.value = table.row(this.parentElement.parentElement).data().name;
+    editLastName.value = table.row(this.parentElement.parentElement).data().surname;
+    editEmail.value = table.row(this.parentElement.parentElement).data().email;
+    editCountry.value = table.row(this.parentElement.parentElement).data().country;
+    editCity.value = table.row(this.parentElement.parentElement).data().city;
+    
+    function updateChanges(e) {
+      e.preventDefault();
       data[index] = {
         id: id,
         name: editFirstName.value,
@@ -103,15 +103,15 @@ $(document).ready(function () {
       };
 
       localStorage.setItem("data", JSON.stringify(data));
-      this.reset();
+      table.clear().rows.add(data).draw(false);
+      $("#editModal .close").click();
     }
-    updateBtn.addEventListener("click", updateChanges);
+    $(".update-changes").on("click", updateChanges);
   }
 
   //delete datatable entry
   function deleteEntry() {
-    let id =
-      this.parentElement.parentElement.parentElement.firstChild.textContent;
+    let id = table.row(this.parentElement.parentElement).data().id;
     let index;
     data.forEach((item) => {
       if (item.id == id) {
@@ -120,23 +120,21 @@ $(document).ready(function () {
     });
     data.splice(index, 1);
     localStorage.setItem("data", JSON.stringify(data));
-
-    $("#example").on("click", ".delete", function () {
-      var table = $("#example").DataTable();
-      table.row($(this).parents("tr")).remove().draw();
-    });
+    table.row(this.parentElement.parentElement).remove().draw(false);
   }
 
-  editBtn.forEach((item) => item.addEventListener("click", callUpdate));
-  deleteBtn.forEach((item) => item.addEventListener("click", deleteEntry));
+  $("#example").on("click", "tbody tr td .btn-group .edit", callUpdate);
+  $("#example").on("click", "tbody tr td .btn-group .delete", deleteEntry);
+  $("#example").on("click", "tbody tr td .btn-group .edit", dropdownOptions)
 
   //fetch data from json file than pass it to giveOptions function
   fetch(endpoint)
     .then((res) => res.json())
     .then((data) => giveOptions(data));
-
   //dependent dropdown
+  //just replace the x,y,z variables for someting more meaningful for more readability  
   function giveOptions(countries) {
+    obj = countries;
     for (var x in countries) {
       selectCountry.options[selectCountry.options.length] = new Option(x, x);
     }
@@ -149,20 +147,36 @@ $(document).ready(function () {
           city[y]
         );
       }
-    };
+    }; 
+  }
 
-    for (var z in countries) {
-      editCountry.options[editCountry.options.length] = new Option(z, z);
+function dropdownOptions(){
+  
+      for (var country in obj) {
+      editCountry.options[editCountry.options.length] = new Option(country, country);
     }
-    editCountry.onchange = function () {
+      
       editCity.length = 1;
-      let city = countries[this.value];
-      for (var w in city) {
+      let city = obj[table.row(this.parentElement.parentElement).data().country];
+      for (let c=0; c<city.length; c++) {
         editCity.options[editCity.options.length] = new Option(
-          city[w],
-          city[w]
+          city[c],
+          city[c]
         );
       }
-    };
-  }
+
+      editCountry.onchange = function () {
+        editCity.length = 1;
+        let city = obj[this.value];
+        for (var y in city) {
+          editCity.options[editCity.options.length] = new Option(
+            city[y],
+            city[y]
+          );
+        }
+      };
+
+      editCity.value = table.row(this.parentElement.parentElement).data().city;
+}
+
 });
